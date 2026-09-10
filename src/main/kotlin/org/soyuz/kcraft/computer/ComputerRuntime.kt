00f55990692f341ff644888c6017fd018223aeea
soyuz-dev <avon2ramesh@gmail.com>
2026-09-10 11:14:56 +0800

    kcraft: rise of the runtime

--- src/main/kotlin/org/soyuz/kcraft/computer/ComputerRuntime.kt
+++ src/main/kotlin/org/soyuz/kcraft/computer/ComputerRuntime.kt
@@ -1,4 +1,32 @@
 package org.soyuz.kcraft.computer
 
-class ComputerRuntime {
+class ComputerRuntime(
+    val terminal: Terminal,
+    val fileSystem: FileSystem
+) {
+    var workingDirectory: String = "/"
+        private set
+
+
+    fun changeDirectory(path: String): Boolean {
+        val normalized = fileSystem.normalizePath(
+            workingDirectory,
+            path
+        )
+
+        if (!fileSystem.exists(normalized)) {
+            return false
+        }
+
+        if (!fileSystem.isDirectory(normalized)) {
+            return false
+        }
+
+        workingDirectory = normalized
+        return true
+    }
+
+    fun openNano(path: String) {
+        // later: switch runtime/screen mode
+    }
 }
